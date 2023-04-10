807ff7d80a140f6a48f0d9905a6c10704286c05a
carthorseZ <cartman.david@gmail.com>
2023-04-10 15:39:10 +1200

    Version 0.1

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from flask import Flask, render_template
+from . import app
+
+@app.route("/")
+def home():
+    return render_template("home.html")
---
+++ readme.txt
@@ -0,0 +1,5 @@
+pip install -r requirements.txt
+
+# macOS/Linux
+# You may need to run `sudo apt-get install python3-venv` first on Debian-based OSs
+python3 -m venv .venv
